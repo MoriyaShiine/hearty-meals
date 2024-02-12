248f13ecba68e420f0f2ef088ffc56a19050cd3c
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2024-02-12 16:05:59 -0600

    fix crash with status effect bars

--- src/main/java/moriyashiine/heartymeals/mixin/client/AbstractInventoryScreenMixin.java
+++ src/main/java/moriyashiine/heartymeals/mixin/client/AbstractInventoryScreenMixin.java
@@ -9,19 +9,15 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import moriyashiine.heartymeals.client.HeartyMealsClient;
 import moriyashiine.heartymeals.common.init.ModStatusEffects;
-import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.AbstractInventoryScreen;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-import java.util.Iterator;
 import java.util.List;
 
 @Mixin(AbstractInventoryScreen.class)
@@ -34,9 +30,10 @@ public class AbstractInventoryScreenMixin {
 		return HeartyMealsClient.prioritizeCozy(instance, elements, original);
 	}
 
-	@Inject(method = "drawStatusEffectBackgrounds", at = @At(value = "INVOKE_ASSIGN", target = "Ljava/util/Iterator;next()Ljava/lang/Object;", shift = At.Shift.BY, by = 2), locals = LocalCapture.CAPTURE_FAILHARD)
-	private void heartymeals$trackCozy(DrawContext context, int x, int height, Iterable<StatusEffectInstance> statusEffects, boolean wide, CallbackInfo ci, int i, Iterator<?> var7, StatusEffectInstance statusEffectInstance) {
+	@ModifyVariable(method = "drawStatusEffectBackgrounds", at = @At("STORE"))
+	private StatusEffectInstance heartymeals$trackCozy(StatusEffectInstance statusEffectInstance) {
 		isCozy = statusEffectInstance.getEffectType() == ModStatusEffects.COZY;
+		return statusEffectInstance;
 	}
 
 	@ModifyArg(method = "drawStatusEffectBackgrounds", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V"))
