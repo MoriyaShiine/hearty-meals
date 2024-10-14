4a76fffe6ce75e5ba4aa3d9f613a9a621aa107e8
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2024-10-14 17:36:14 -0500

    fix natural regen sync applying to all gamerules

--- src/main/java/moriyashiine/heartymeals/mixin/GameRulesBooleanRuleMixin.java
+++ src/main/java/moriyashiine/heartymeals/mixin/GameRulesBooleanRuleMixin.java
@@ -8,18 +8,15 @@ import moriyashiine.heartymeals.client.payload.SyncNaturalRegenPayload;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.world.GameRules;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameRules.BooleanRule.class)
 public class GameRulesBooleanRuleMixin {
-	@Shadow
-	private boolean value;
-
 	@Inject(method = "setFromArgument", at = @At("TAIL"))
 	private void heartymeals$syncNaturalRegen(CommandContext<ServerCommandSource> context, String name, CallbackInfo ci) {
-		context.getSource().getServer().getPlayerManager().getPlayerList().forEach(foundPlayer -> SyncNaturalRegenPayload.send(foundPlayer, value));
+		boolean naturalRegen = context.getSource().getServer().getGameRules().getBoolean(GameRules.NATURAL_REGENERATION);
+		context.getSource().getServer().getPlayerManager().getPlayerList().forEach(foundPlayer -> SyncNaturalRegenPayload.send(foundPlayer, naturalRegen));
 	}
 }
