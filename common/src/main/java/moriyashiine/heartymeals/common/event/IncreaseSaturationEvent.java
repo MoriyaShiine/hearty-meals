f13c0b3271d59105e6982bd0324a5657f655e8b2
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2026-09-05 19:11:51 -0500

    high saturation food no longer has ridiculously fast heal times, and the unique ingredient bonus now applies to nutrition instead of saturation

--- common/src/main/java/moriyashiine/heartymeals/common/event/IncreaseSaturationEvent.java
+++ common/src/main/java/moriyashiine/heartymeals/common/event/IncreaseSaturationEvent.java
@@ -2,20 +2,38 @@ package moriyashiine.heartymeals.common.event;
 
 import moriyashiine.heartymeals.common.tag.HeartyMealsItemTags;
 import moriyashiine.strawberrylib.api.event.FoodEvents;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
-public class IncreaseSaturationEvent implements FoodEvents.ModifySaturation {
+public class IncreaseSaturationEvent {
 	public static void init() {
-		FoodEvents.MODIFY_SATURATION.register(new IncreaseSaturationEvent());
+		FoodEvents.MODIFY_NUTRITION.register(new Nutrition());
+		FoodEvents.MODIFY_SATURATION.register(new Saturation());
 	}
 
-	@Override
-	public float modify(float saturation, Level level, Player user, ItemStack stack) {
-		if (stack.is(HeartyMealsItemTags.INCREASED_SATURATION)) {
-			saturation *= 2.6F;
+	private static class Nutrition implements FoodEvents.ModifyNutrition {
+		@Override
+		public int modify(int nutrition, Level level, Player user, ItemStack stack) {
+			int uniqueIngredientBonus = Mth.floor(UniqueIngredientsEvent.getUniqueIngredients(stack.getItem()) / 2F);
+			return nutrition + uniqueIngredientBonus;
 		}
-		return saturation + UniqueIngredientsEvent.getUniqueIngredients(stack.getItem()) / 2F;
+	}
+
+	private static class Saturation implements FoodEvents.ModifySaturation {
+		// Golden Carrot Saturation
+		private static final float MAX_SATURATION = 14.4F;
+
+		@Override
+		public float modify(float saturation, Level level, Player user, ItemStack stack) {
+			if (stack.is(HeartyMealsItemTags.INCREASED_SATURATION)) {
+				saturation *= 3;
+			}
+			float cappedSaturation = Math.min(MAX_SATURATION, saturation);
+			float extraSaturation = Math.max(0, saturation - MAX_SATURATION);
+			float extraSaturationBonus = Math.max(0, (float) Math.log(extraSaturation));
+			return cappedSaturation + extraSaturationBonus;
+		}
 	}
 }
